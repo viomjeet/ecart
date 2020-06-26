d8e3f4e941752a49fb290a9f4f3818d0b3c647a5
vikas kumar <viomjeet@gmail.com>
2020-06-26 22:56:28 +0530

    Updates

---
+++ precache-manifest.24c16738a5be52a3fee88593e5888c6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4d32937db937efa16b0f0b696c6150",
+    "url": "/ecart/index.html"
+  },
+  {
+    "revision": "57ef672726497b2370f8",
+    "url": "/ecart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aec6bf9493356c72058d",
+    "url": "/ecart/static/css/main.c9cc367e.chunk.css"
+  },
+  {
+    "revision": "57ef672726497b2370f8",
+    "url": "/ecart/static/js/2.17d726d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecart/static/js/2.17d726d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec6bf9493356c72058d",
+    "url": "/ecart/static/js/main.f0800052.chunk.js"
+  },
+  {
+    "revision": "e44629b955c74d140386",
+    "url": "/ecart/static/js/runtime-main.3bb36bbd.js"
+  },
+  {
+    "revision": "a9a37721e9ac585ad9889a78f8b061b0",
+    "url": "/ecart/static/media/0001.a9a37721.jpg"
+  },
+  {
+    "revision": "ab3709801edf492abb702cea26dd6d53",
+    "url": "/ecart/static/media/0002.ab370980.jpg"
+  },
+  {
+    "revision": "c82e2c1cdcce21fe733ed303b6098c44",
+    "url": "/ecart/static/media/0003.c82e2c1c.jpg"
+  }
+]);
